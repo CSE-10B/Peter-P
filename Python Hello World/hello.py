78e3c003522de850e6ca46549c952cdab96a74f1
PeterPari <parizhskypete@gmail.com>
2026-02-13 15:16:29 -0500

    Hello World Assignment

---
+++ Python Hello World/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+hello.py — simple Hello World example for CSE-10B
+Run:
+    python3 "Python Hello World/hello.py"
+"""
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
