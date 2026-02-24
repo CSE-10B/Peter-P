51e8a34542ec8a63859dfe7ab74e48cb136b4af4
Peter P <pparizhsky@browning.edu>
2026-02-24 16:31:01 +0000

    Hello world

--- Python Hello World/hello.py
+++ Python Hello World/hello.py
@@ -1,13 +1,1 @@
-#!/usr/bin/env python3
-"""
-hello.py — simple Hello World example for CSE-10B
-Run:
-    python3 "Python Hello World/hello.py"
-"""
-
-def main():
-    print("Hello, World!")
-
-
-if __name__ == "__main__":
-    main()
+print("Hello, World!")
